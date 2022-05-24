5532f7785b4d77f07a5cd644cfb65924d6140549
Zied BEN TAHAR <bentaharzied@gmail.com>
2022-05-24 13:49:33 +0200

    notify success via ws

--- src/backend/lambdas/notify-success/index.ts
+++ src/backend/lambdas/notify-success/index.ts
@@ -3,12 +3,16 @@ import AWS from "aws-sdk";
 import { buildArticleAudioKey } from "../shared/keyBuilder";
 
 const s3 = new AWS.S3({ region: process.env.AWS_REGION });
+const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+  apiVersion: "2018-11-29",
+  endpoint: process.env.WS_NOTIFICATION_ENDPOINT,
+});
 
 export const handler = async (
   event: { articleKey: string; connectionId: string },
   context: Context
 ): Promise<{ audioUrl: string }> => {
-  const { articleKey } = event;
+  const { articleKey, connectionId } = event;
 
   const storageParams = {
     Bucket: process.env.CONTENT_REPO_BUCKET_NAME!,
@@ -16,6 +20,13 @@ export const handler = async (
   };
 
   const audioUrl = await s3.getSignedUrlPromise("getObject", storageParams);
+
+  await apigwManagementApi
+    .postToConnection({
+      ConnectionId: connectionId,
+      Data: JSON.stringify({ status: "success", audioUrl }),
+    })
+    .promise();
 
   return {
     audioUrl,
