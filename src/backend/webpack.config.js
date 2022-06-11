764d4d72c56e442b5ec2688ba1ae057d5c635035
Zied BEN TAHAR <bentaharzied@gmail.com>
2022-06-11 16:32:44 +0200

    infra: custom domain name for ws api

--- src/backend/webpack.config.js
+++ src/backend/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: {
